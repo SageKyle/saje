ad2540c572fa6a239734759e6a37187cd65086ed
SageKyle <ominyipaulo@gmail.com>
2023-11-02 12:50:40 +0100

    update styles for the hero section of home

--- app/components/home/Hero.tsx
+++ app/components/home/Hero.tsx
@@ -1,4 +1,4 @@
-import Avatar from '@/app/assets/images/saje_avatar.png'
+import Avatar from '@/app/assets/images/saje-avatar.png'
 import Style from '@/app/assets/styles/hero.module.css'
 import Image from 'next/image'
 import { AiFillLinkedin } from 'react-icons/ai'
@@ -12,67 +12,62 @@ export default function Hero() {
 			<section aria-label="hero section" className={Style.container}>
 				<Navbar />
 				<div className={Style.hero}>
-					<article>
-						<h1>Frontend Developer.</h1>
-						<br />
-						{/* <p>
-							I like to craft solid and scalable frontend products with great
-							user experiences.
-						</p> */}
-						<p>
+					<article className={Style.heroTxt}>
+						<h1 className={Style.title}>
+							Frontend Developer<span>.</span>
+						</h1>
+						<p className={Style.intro}>
 							In a world of pixels and code, I&apos;m the frontend alchemist who
 							transforms your ideas into web gold. From JavaScript sorcery to
 							Next.js wizardry, I&apos;m here to make it magical.
 						</p>
 					</article>
-
-					<Image
-						src={Avatar}
-						alt="Paul Saje avatar"
-						loading="lazy"
-						width={300}
-						height={350}
-					/>
+					<section className={Style.heroImg}>
+						<Image src={Avatar} alt="Paul Saje avatar" loading="lazy" fill />
+					</section>
 				</div>
-				<div
-					className={Style.socialGroup}
-					aria-description="social media icons"
-				>
-					<a
-						href="https://twitter.com/Ominyi_Saje"
-						target="_blank"
-						title="X (Twitter)"
-						rel="noopener noreferrer"
-						className={Style.socialItem}
+				<article className={Style.heroFooter}>
+					<div
+						className={Style.socialGroup}
+						aria-description="social media icons"
 					>
-						<FaXTwitter />
-					</a>
-					<a
-						href="https://linkedin.com/in/ominyi"
-						target="_blank"
-						title="LinkedIn"
-						rel="noopener noreferrer"
-						className={Style.socialItem}
-					>
-						<AiFillLinkedin />
-					</a>
-					<a
-						href="mailto:ominyi.webdev@gmail.com"
-						title="Send me a mail"
-						className={Style.socialItem}
-					>
-						<MdEmail />
-					</a>
-					<a
-						href="https://saje.hashnode.dev"
-						target="_blank"
-						title="My blog"
-						rel="noopener noreferrer"
-						className={Style.socialItem}
-					>
-						<FaHashnode />
-					</a>
-				</div>
+						<a
+							href="https://twitter.com/Ominyi_Saje"
+							target="_blank"
+							title="X (Twitter)"
+							rel="noopener noreferrer"
+							className={Style.socialItem}
+						>
+							<FaXTwitter />
+						</a>
+						<a
+							href="https://linkedin.com/in/ominyi"
+							target="_blank"
+							title="LinkedIn"
+							rel="noopener noreferrer"
+							className={Style.socialItem}
+						>
+							<AiFillLinkedin />
+						</a>
+						<a
+							href="mailto:ominyi.webdev@gmail.com"
+							title="Send me a mail"
+							className={Style.socialItem}
+						>
+							<MdEmail />
+						</a>
+						<a
+							href="https://saje.hashnode.dev"
+							target="_blank"
+							title="My blog"
+							rel="noopener noreferrer"
+							className={Style.socialItem}
+						>
+							<FaHashnode />
+						</a>
+					</div>
+					<div className={Style.ScrollAnim} aria-hidden></div>
+				</article>
 			</section>
 		</>
 	)
