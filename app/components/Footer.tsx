13c9837355360c028f0194d89237bc67eece185d
SageKyle <ominyipaulo@gmail.com>
2023-11-06 17:58:33 +0100

    setup attribute for toggling animations

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,14 +1,18 @@
+import Styles from '@/app/assets/styles/footer.module.css'
+
 export default function Footer() {
 	return (
-		<footer>
-			<p>&copy; Paul Saje 2023</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
-				necessitatibus ipsum, hic natus doloremque consequuntur animi molestiae
-				maxime ducimus soluta suscipit incidunt ipsam aspernatur, quisquam
-				obcaecati unde, qui voluptatibus? Aliquid aut eos aspernatur, fugiat
-				iusto provident maiores. Iste, unde veritatis.
-			</p>
+		<footer id="footer" className={Styles.wrapper}>
+			<section className={Styles.container}>
+				<p>&copy; Paul Saje 2023</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+					necessitatibus ipsum, hic natus doloremque consequuntur animi
+					molestiae maxime ducimus soluta suscipit incidunt ipsam aspernatur,
+					quisquam obcaecati unde, qui voluptatibus? Aliquid aut eos aspernatur,
+					fugiat iusto provident maiores. Iste, unde veritatis.
+				</p>
+			</section>
 		</footer>
 	)
 }
