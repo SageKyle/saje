13b9943e4fc05de9be944b91d6ba9cbe0e915d52
SageKyle <ominyipaulo@gmail.com>
2023-11-20 22:14:46 +0100

    [experimental] setup emailing using resend
    BREAKING CHANGES
    accessing the API key from`.env.local` is currently not working
    
    - using it directly in `app/api/send/route.ts` is returning a `CORS access error`

---
+++ app/components/email-template.tsx
@@ -0,0 +1,21 @@
+export type EmailTemplateProps = {
+	name: string
+	email: string
+	message: string
+}
+
+export function EmailTemplate(
+	props: Readonly<EmailTemplateProps>
+): JSX.Element {
+	return (
+		<section>
+			<h1>New form submission from {props.name}</h1>
+			<p>
+				Hey Saje, you have a new message on your portfolio site from{' '}
+				{props.name}
+			</p>
+			<small>Message: {props.message}</small>
+			<p>Email address: {props.email}</p>
+		</section>
+	)
+}
